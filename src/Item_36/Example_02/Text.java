bdb55af87f7c66a15db4a7b923102c42daac5add
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-12-11 13:00:05 +0100

    Item 36 updated

--- src/Item_36/Example_02/Text.java
+++ src/Item_36/Example_02/Text.java
@@ -4,6 +4,7 @@ import java.util.Set;
 
 //EnumSet - a modern replacement for bit fields
 public enum Text {
+    INSTANCE;
     public enum Style {BOLD, ITALIC, UNDERLINE, STRIKETHROUGH}
 
     //Any Set could be passed in, but EnumSet is clearly best
