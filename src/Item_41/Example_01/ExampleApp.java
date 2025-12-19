acdd5a6bf67ec7d08f2ab89f9f0532155cc0471b
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2025-12-19 09:05:26 +0100

    item 41 comments added

--- src/Item_41/Example_01/ExampleApp.java
+++ src/Item_41/Example_01/ExampleApp.java
@@ -14,5 +14,22 @@ public class ExampleApp {
 
         // We can't do the same thing with annotation! + annotation work only at runtime.
         // Marker interface work at compile time!
+
+        /*
+        Bloch’s key idea (simplified)
+
+            no - Do not use a marker interface as a flag
+            no - Do not use it only for runtime checks
+            yes - Use it to restrict APIs and types at compile time
+
+        Mini-checklist (very useful)
+
+            1 Before creating a marker interface, ask yourself:
+            2 Will it be used as a parameter type or a generic bound?
+            3 Does it prevent incorrect usage at compile time?
+            4 Does it define a clear semantic contract?
+
+        If the answer to any of these questions is “no” → a marker interface is a bad idea.
+         */
     }
 }
