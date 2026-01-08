6ba159f579404b427976fa8a0bc199f4e95fcf16
SerebrovskiyV <vasiliy.serebrovskiy@gmail.com>
2026-01-08 16:48:05 +0100

    Item 45 examples added

---
+++ src/Item_45/Example_02/Anagrams.java
@@ -0,0 +1,35 @@
+package Item_45.Example_02;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.groupingBy;
+
+/**
+ * @author Vasilii Serebrovskii
+ * @version 1.0 (08.01.2026)
+ */
+// Overuse of streams - don't do this!
+public class Anagrams {
+
+    public static void main(String[] args) throws IOException {
+
+        Path dictionary = Paths.get(args[0]);
+        int minGroupSize = Integer.parseInt(args[1]);
+
+        try(Stream<String> words = Files.lines(dictionary)) {
+            words.collect(
+                    groupingBy(word -> word.chars().sorted()
+                            .collect(StringBuilder::new,
+                                    (sb, c) -> sb.append((char) c),
+                                    StringBuilder::append).toString()))
+                    .values().stream()
+                        .filter(group -> group.size() < minGroupSize)
+                    .map(group -> group.size() + ": " + group)
+                    .forEach(System.out::println);
+        }
+    }
+}
